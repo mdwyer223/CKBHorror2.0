76326d0245a79d36f902da80dad86dd4fc348f83
Matthew Dwyer <mdwyer223@aol.com>
2014-10-21 12:49:39 -0400

    Merged Dave's and matt's files

--- CKB/CKB/CKB/Rooms/Floor.cs
+++ CKB/CKB/CKB/Rooms/Floor.cs
@@ -9,7 +9,35 @@ using Microsoft.Xna.Framework.Content;
 
 namespace CKB
 {
-    class Floor
+    public class Floor
     {
+        //list of objects
+        protected Color color;
+        Texture2D background;
+        Vector2 position;
+        Rectangle drawingRec;
+        int width, height;
+
+        public Floor(Texture2D background, Vector2 position, int width, int height)
+            : base()
+        {
+            this.background = background;
+            this.position = position;
+            this.width = width;
+            this.height = height;
+
+            drawingRec = new Rectangle((int)position.X, (int)position.Y, width, height);
+        }
+
+        public virtual void update(GameTime gameTime)
+        {
+            //updates everything
+        }
+
+        public virtual void draw(SpriteBatch spriteBatch)
+        {
+            //draws everything
+            spriteBatch.Draw(background, drawingRec, color);
+        }
     }
 }
